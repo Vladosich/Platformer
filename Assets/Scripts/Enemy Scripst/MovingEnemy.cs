f7b7a170c0226e96fe6a1c2ddbee769176006919
Vladosich <vladislavgockaluk@gmail.com>
2022-04-27 15:46:58 +0300

    Движение врага
    Движение врага в обратную сторону при столкновении с объектами и поворот спрайта

--- Assets/Scripts/Enemy Scripst/MovingEnemy.cs
+++ Assets/Scripts/Enemy Scripst/MovingEnemy.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class MovingEnemy : Enemy
 {
-    [SerializeField] private Vector2 direction;
+    private Vector2 direction = new Vector2(0.01f, 0f);
+
+    private readonly float xDirection = 0.01f;
+
+    private SpriteRenderer enemySprite;
+
+    private void Start()
+    {
+        enemySprite = GetComponentInChildren<SpriteRenderer>();
+    }
 
     private void FixedUpdate()
     {
@@ -12,10 +21,29 @@ public class MovingEnemy : Enemy
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
         base.OnCollisionEnter2D(collision);
+
+        if(!collision.gameObject.CompareTag("Player") && !collision.gameObject.CompareTag("Ground"))
+        {
+            if(direction.x > 0)
+            {
+                direction.x = -xDirection;
+                Flip();
+            }
+            else
+            {
+                direction.x = xDirection;
+                Flip();
+            }
+        }
     }
 
     private void Walk()
     {
         transform.Translate(direction);
+    }
+
+    private void Flip()
+    {
+        enemySprite.flipX = !enemySprite.flipX;
     }
 }
